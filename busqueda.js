8d5cf0700f06edb14fc4ffd6cf6c254b0a1e231b
SantanaRosas <106643590+SantanaRosas@users.noreply.github.com>
2022-11-30 12:25:36 -0600

    Add files via upload

--- busqueda.js
+++ busqueda.js
@@ -11,3 +11,13 @@ document.addEventListener("keyup", e =>{
     }
 
 })
+// metodo de temas
+const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
+            function cambiaTema(ev){
+                if(ev.target.checked){
+                    document.documentElement.setAttribute('tema', 'light');
+                } else {
+                    document.documentElement.setAttribute('tema', 'dark');
+                }
+            }
+            colorSwitch.addEventListener('change', cambiaTema);
